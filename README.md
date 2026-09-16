30060cc585864acda287d88058a66da16abee57d
marcelooluz <marcelo.luz.bento@gmail.com>
2026-09-16 14:46:14 -0300

    feat: adiciona o fluxo de cliques e digitação automática com pyautogui

---
+++ README.md
@@ -0,0 +1,13 @@
+# ✉️ Automação de E-mails com PyAutoGUI
+
+Projeto simples em Python que utiliza automação visual (cliques de mouse e atalhos de teclado) para enviar e-mails de forma automatizada pelo navegador.
+
+## 🚀 Tecnologias Utilizadas
+- **Python 3**
+- **PyAutoGUI**: Simulação de eventos de mouse e teclado
+- **Pyperclip**: Manipulação da área de transferência para preservar acentuação
+
+## 📌 Como Executar
+1. Instale as dependências:
+   ```bash
+   pip install pyautogui pyperclip
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import webbrowser
 import pyautogui
+import pyperclip
 import time
 
 # Dados dos e-mails para enviar
 lista_contatos = [
-    {"email": "exemplo1@gmail@com", "assunto": "Relatório Semanal", "mensagem": "Segue o relatório atualizado!"}    
+    {
+        "email": "marcelo.luz.bento@gmail.com", 
+        "assunto": "Relatório Semanal",
+        "mensagem": "Olá! Segue a atualização da semana com acentos: atenção, confirmação."
+        }    
 ]
 print("Iniciando automação . . .")
 time.sleep(2)
@@ -14,3 +19,47 @@ webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
 
 # Tempo para o navegador abrir e carregar a tela 
 time.sleep(5)
+
+for contato in lista_contatos:
+    #  Clica no botão 'Escrever'
+    pyautogui.click(x=103, y=161)
+    time.sleep(3)
+    
+    # Digita o e-mail do destinatário
+    pyautogui.write(contato["email"])
+    pyautogui.press('enter')
+    time.sleep(1)
+    
+    # Vai para o campo Assunto (pressiona TAB)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyperclip.copy(contato["assunto"])
+    
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('v')
+    pyautogui.keyUp('ctrl')
+    time.sleep(1)
+    
+    #  Vai para o campo 'Corpo da Mensagem' (Pressiona TAB)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyperclip.copy(contato["mensagem"])
+    
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('v')
+    pyautogui.keyUp('ctrl')
+    time.sleep(1)
+    
+    # Envia o e-mail (Atalho padrão do Gmail para enviar: crtl + enter)
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('enter')
+    pyautogui.keyUp('ctrl')
+        
+    time.sleep(3)
+    
+print("Todos os emails foram enviados com sucesso!")
+    
+    
+    
+    
+    
